76d8933e165fa0286470843bb1363684302a5ae9
lionel5116 <lioneljones5116@gmail.com>
2020-05-26 13:53:01 -0500

    more code from training

--- src/components/FetchDataPureFunction.js
+++ src/components/FetchDataPureFunction.js
@@ -3,23 +3,44 @@ import axios from "axios";
 import { Container } from "react-bootstrap";
 
 function FetchDataPureFunction() {
-  const [axiosRecs, setAxiosRecords] = useState([]);
+  //const [axiosRecs, setAxiosRecords] = useState([]);  //use this when you want to render more than one item
+  const [axiosRecs, setAxiosRecord] = useState({});
+  const [id, setId] = useState(1);
 
+  /*
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
       setAxiosRecords(res.data);
     });
   },[]);
+ */
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setAxiosRecord(res.data);
+      });
+  }, [id]);
 
   return (
     <Container style={myStyles.centerContainerContent}>
       <div>
-        <h2>List of Axios Records</h2>
-        {axiosRecs.length
-          ? axiosRecs.map((axisRecord) => (
-              <div key={axisRecord.id}>{axisRecord.title}</div>
-            ))
-          : null}
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <h2>List of Axios Record(s)</h2>
+        {axiosRecs.length ? (
+          axiosRecs.map((axisRecord) => (
+            <div key={axisRecord.id}>{axisRecord.title}</div>
+          ))
+        ) : (
+          <div>{axiosRecs.title}</div>
+        )}
       </div>
     </Container>
   );
